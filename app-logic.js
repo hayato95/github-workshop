692e8698964e88acf592864790954be054e30175
yoyoman-dot <310.youtaro@gmail.com>
2026-09-06 15:17:57 +0900

    feat: 運勢を判定するgetFortuneを実装

--- app-logic.js
+++ app-logic.js
@@ -24,7 +24,8 @@ function getLuckyNumber(index) {
 // 例: getFortune(0) は何らかの文字列(例 "大吉")を返す
 // ------------------------------------------------------------
 function getFortune(index) {
-  throw new Error('TODO: 実装してください (担当A: getFortune)');
+const FORTUNES = ['神ってる', '絶好調', 'まあまあ', 'ぼちぼち', '今日は寝よう'];
+  return FORTUNES[index];
 }
 
 // ------------------------------------------------------------
